35aaae25471266c2accc0f37637edee7575ceb61
Federico Marchini <federico.marchini@accenture.com>
2020-06-04 19:51:14 -0300

    First commit

---
+++ Primer Parcial/MiParcial/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MiParcial
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Primer Parcial/MiParcial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiParcial.MvcApplication" Language="C#" %>
